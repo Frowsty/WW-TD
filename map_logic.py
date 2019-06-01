97e9e8cfa9a0290400e69da5f5260c39b657a18e
Psychicash <gnitsaf@gmail.com>
2019-06-01 16:56:13 -0400

    map has basic functionality

---
+++ map_logic.py
@@ -0,0 +1,265 @@
+import random
+import pygame
+from pygame import freetype
+import wang
+import a_star
+import os
+from os import path
+
+
+#initializers
+if __name__ == 'main':
+    pygame.init()
+if not pygame.display.get_init():
+    pygame.display.init()
+if not pygame.freetype.was_init():
+    pygame.freetype.init()
+if not pygame.mixer.get_init():
+    pygame.mixer.init()
+
+#constants
+_Multiplier = 1
+SCREEN_WIDTH = 1280 * _Multiplier
+SCREEN_HEIGHT = 1024 * _Multiplier
+FPSCLOCK = pygame.time.Clock() #sets up a clock used for throttleing the fps
+BOARDWIDTH = 12
+BOARDHEIGHT = 7
+TILESIZE = 100
+FPS = 60
+BLANK = None
+DEBUG = False
+
+
+#color constants
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+GREEN = (12, 255, 0)
+DK_GREEN = (51, 102, 0)
+BLUE = (18, 0, 255)
+ORANGE = (255, 186, 0)
+SKYBLUE = (39, 145, 251)
+PURPLE = (153, 51, 255)
+DK_PURPLE = (102, 0, 204)
+BROWN = (204, 153, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+#GLOBALS
+_Fullscreen_flag = False
+
+
+
+
+
+_image_library = {}
+
+def get_image(path):
+    global _image_library
+    image = _image_library.get(path)
+    if image == None:
+        canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
+        image = pygame.image.load(canonicalized_path)
+    return image
+
+#follow the rabit hole, all classes run their calls to functions in game and main talks to game
+
+def screen_Grid(screen, columns, rows,_Multiplier):
+
+    width = 80 *_Multiplier
+    height = 80 *_Multiplier
+    margin = round(2 *_Multiplier)
+    upper_Left_X = screen.get_width() - (((columns +1) * width) + ((columns + 2) * margin))
+    upper_Left_Y = screen.get_height() - (((rows + 2) * height) + ((rows + 2) * margin))
+    lower_Right_Y = upper_Left_Y + (rows * height) + ((rows + 1) * margin)
+    lower_Right_X = upper_Left_X + (columns * width) + (columns * margin)
+    lower_Left_Y = lower_Right_Y
+    lower_Left_X = upper_Left_X
+    upper_Right_X = lower_Right_X
+    upper_Right_Y = upper_Left_Y
+
+
+
+    pygame.draw.rect(screen, (WHITE), ((upper_Left_X, upper_Left_Y), (lower_Right_X - (height //2) - (margin * 3), lower_Right_Y - (width //2))))
+    pygame.draw.line(screen, BLACK, (upper_Left_X, upper_Left_Y), (upper_Right_X, upper_Right_Y), margin)
+    for row in range(rows):
+        for column in range(columns):
+            pygame.draw.line(screen, BLACK, (upper_Left_X, upper_Left_Y + (row * height) + (row * margin)),
+                             (upper_Right_X, upper_Right_Y + (row * height) + (row * margin)))  # horizontal line
+            pygame.draw.line(screen, BLACK, (upper_Left_X + (column * width) + (column * margin), upper_Left_Y), ((upper_Left_X + (column * width) + (column * margin) , lower_Left_Y)))  # vertical line
+            if DEBUG:
+                print("Upper x coord")
+                print(upper_Left_X + (column * width) + (column * margin))
+                print("Upper y coord")
+                print(upper_Left_Y)
+                print("Lower x coord")
+                print((upper_Left_X + (column * width) + (column * margin)))
+                print("Lower y coord")
+                print(lower_Left_Y)
+                print("other y is")
+                print((upper_Right_X, upper_Right_Y + (row * height) + (row * margin)))
+
+    pygame.draw.line(screen, BLACK, (upper_Right_X, upper_Right_Y), (lower_Right_X, lower_Right_Y), margin)
+    pygame.draw.line(screen, BLACK, (lower_Left_X, lower_Left_Y), (lower_Right_X, lower_Right_Y), margin)
+
+
+
+
+
+
+
+
+class node(pygame.sprite.Sprite):
+    def __init__(self, x, y, img, screen, _Multiplier):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        self.image = get_image(img)
+        self.images.append(self.image)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self):
+        pass
+
+    def return_Coords(self):
+        return (self.rect.centerx, self.rect.centery)
+
+
+class road():
+    def __init__(self, screen, _Multiplier, start_x, start_y):
+        self.starting_x = start_x
+        self.starting_y = start_y
+        self.road_Points = []
+        self.road_Points.append((self.starting_x, self.starting_y))
+        self.x = self.starting_x
+        self.y = self.starting_y
+        self.moving()
+
+    def moving(self):
+        move_y = random.randint(16, 27)
+        move_x = random.randint(16, 27)
+        self.x -= move_x
+        if move_y % 2 == 0:
+            self.y -= move_y
+        else:
+            self.y += move_y
+
+        if self.y > (778 + 80):
+            self.y -= (move_y * 2)
+        elif self.y < 42:
+            self.y += (move_y * 2)
+
+        if self.x < 122:
+            self.x = 122
+
+        self.road_Points.append((self.x, self.y))
+        return self.road_Points[-1][0], self.road_Points[-1][1]
+
+
+
+
+
+
+
+
+
+
+
+def generate_Map(screen, _Multiplier, map_Sprite_Group):
+    map_Grid = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+    margin = 2
+    num_Of_Rows = 10
+    num_Of_Col = 14
+
+    start_x = 1114
+
+    selection = [40, 122, 204, 286, 368, 450, 532, 614, 696, 778]
+
+    start_y = random.choice(selection)
+
+
+    start = node(start_x, start_y, './images/placeholder/start.png', screen, _Multiplier)
+
+    map_Sprite_Group.add(start)
+
+    o_Trail = road(screen, _Multiplier, start_x, start_y)
+    trail_Nodes = []
+    trail_Nodes.append(o_Trail.moving())
+
+    while trail_Nodes[-1][0] > 122:
+        trail_Nodes.append(o_Trail.moving())
+
+
+    end_Point = node(50, trail_Nodes[-1][1], './images/placeholder/end.png', screen, _Multiplier)
+    map_Sprite_Group.add(end_Point)
+
+
+
+
+    return trail_Nodes, end_Point, start
+
+
+
+
+
+
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+map_Sprite_Group = pygame.sprite.Group()
+
+def main(screen, _Multiplier, map_Sprite_Group):
+
+
+
+    pygame.display.set_caption('WW-TS')
+    pygame.mouse.set_visible(True)
+
+    #create our objects and set data
+    done = False
+    all_Sprite_Group = pygame.sprite.Group()
+    trail_Points, end_Point, start = generate_Map(screen, _Multiplier, map_Sprite_Group)
+    
+
+
+    while not done:
+        screen.fill(BLACK)
+        overlay = pygame.Surface((screen.get_width(), screen.get_height()))
+        overlay.set_alpha(150)
+        overlay.fill((WHITE))
+        screen.blit(overlay, (0,0))
+
+
+
+        pygame.event.pump()
+
+        screen_Grid(screen, 14, 10, _Multiplier)
+
+        map_Sprite_Group.draw(screen)
+        pygame.draw.line(screen, DK_PURPLE, (trail_Points[0][0], trail_Points[0][1]), (start.return_Coords()))
+        for i in range(len(trail_Points) - 2):
+           pygame.draw.line(screen, DK_PURPLE, (trail_Points[i][0], trail_Points[i][1]), (trail_Points[i+1][0], trail_Points[i+1][1]) )
+        pygame.draw.line(screen, DK_PURPLE, (trail_Points[-2][0], trail_Points[-2][1]), (end_Point.return_Coords()))
+
+
+        pygame.display.flip()
+
+        FPSCLOCK.tick(60)
+
+
+    pygame.quit() # closes window and exits
+
+if __name__ == '__main__':
+    main(screen,_Multiplier, map_Sprite_Group)
