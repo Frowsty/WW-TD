868fe991e11b64cec67acbe110b4473fe22dadc7
Frowsty <daniel_barbis@hotmail.com>
2019-06-01 21:52:38 +0100

    Some refactoring in the code, added a basic player class with some basic movement <- This will be touched on during the development of this game

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import FroPy as fp
 import pygame
-from main_menu import *
+
+import main_menu as menu
+import game_entities as entities
 
 # Initialize pygame
 pygame.init()
@@ -28,29 +30,29 @@ font = pygame.font.SysFont("Arial", 20)
 start_game = False
 how_to = False
 
-while True:
+# Initialize our player
+player = entities.Player([300, 300])
 
-    # set max ticks per second (FPS)
-    clock.tick(120)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_ESCAPE]:
-            quit()
-
-    mouse_x, mouse_y = pygame.mouse.get_pos()
-
+def draw_gamewindow(screen, mouse_x, mouse_y, kb_input):
+    global start_game, how_to
     screen.fill(BLACK)
 
     # To prevent screen from blinking black we're calling draw_howto first if
     # player choose How To screen in main menu
+
+    if start_game == True:
+        screen.blit(pygame.transform.scale(pygame.image.load("pictures/menu_bg.jpg"), (1280, 960)), (0,0))
+        player.draw(screen)
+        player.movement(screen, kb_input)
+
     if how_to == True:
-        draw_howto(screen, mouse_x, mouse_y, font)
+        menu.draw_howto(screen, mouse_x, mouse_y, font)
 
     # Main menu
-    menu_input = menu_system(mouse_x, mouse_y)
+    menu_input = menu.menu_system(mouse_x, mouse_y)
     if start_game == False and how_to == False:
-        update_mm()
-        draw_mm(screen, mouse_x, mouse_y)
+        menu.update_mm()
+        menu.draw_mm(screen, mouse_x, mouse_y)
     if menu_input == "QUIT":
         quit()
     if menu_input == "START":
@@ -61,5 +63,18 @@ while True:
         how_to = False
         start_game = False
 
+while True:
 
+    # set max ticks per second (FPS)
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_ESCAPE]:
+            quit()
+
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    keyboard_input = pygame.key.get_pressed()
+
+    draw_gamewindow(screen, mouse_x, mouse_y, keyboard_input)
+    
     pygame.display.update()
