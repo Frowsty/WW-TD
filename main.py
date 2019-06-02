d744713642d8cc7b1f7e0f00f9d16bf53cb27d1e
Psychicash <gnitsaf@gmail.com>
2019-06-02 01:16:19 -0400

    merged files

---
+++ main.py
@@ -0,0 +1,138 @@
+import FroPy as fp
+import pygame
+from pygame import freetype
+import random
+import ui_components as ui
+import game_entities as entities
+
+import wang
+import a_star
+import os
+from os import path
+import map_logic
+
+#initializers
+if __name__ == 'main':
+    pygame.init()
+if not pygame.display.get_init():
+    pygame.display.init()
+if not pygame.freetype.was_init():
+    pygame.freetype.init()
+if not pygame.mixer.get_init():
+    pygame.mixer.init()
+
+
+#sprite groups
+all_Sprite_Group = pygame.sprite.Group()
+map_Sprite_Group = pygame.sprite.Group()
+
+
+
+# Define colors
+BLACK = (0, 0, 0)
+GREY = (35, 35, 35)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+YELLOW = (255, 255, 0)
+DK_GREEN = (51, 102, 0)
+ORANGE = (255, 186, 0)
+SKYBLUE = (39, 145, 251)
+PURPLE = (153, 51, 255)
+DK_PURPLE = (102, 0, 204)
+BROWN = (204, 153, 0)
+
+
+# Set screen size
+size = [1280, 960]
+screen = pygame.display.set_mode(size)
+
+#ash's constants
+_Multiplier = 1
+SCREEN_WIDTH = 1280 * _Multiplier
+SCREEN_HEIGHT = 1024 * _Multiplier
+BLANK = None
+DEBUG = False
+Map_Shown = False
+
+
+# Set window title
+pygame.display.set_caption("WW - TD")
+
+# Define Clock
+clock = pygame.time.Clock()
+
+font = pygame.freetype.SysFont("Arial", 20)
+
+start_game = False
+how_to = False
+
+# Initialize our player
+player = entities.Player([300, 300])
+map = map_logic.Game_Map(map_Sprite_Group, _Multiplier, screen)
+
+#ASH - image function, loads image into an array and if it has already been loaded, it loads the previous loaded image
+#instead of wasting memory on a new image. if it hasn't been it loads it.
+_image_library = {}
+
+def get_image(path):
+    global _image_library
+    image = _image_library.get(path)
+    if image == None:
+        canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
+        image = pygame.image.load(canonicalized_path)
+    return image
+
+def draw_gamewindow(screen, mouse_x, mouse_y, kb_input):
+    global start_game, how_to
+    screen.fill(BLACK)
+
+    # To prevent screen from blinking black we're calling draw_howto first if
+    # player choose How To screen in main menu
+
+    if start_game == True:
+        screen.blit(pygame.transform.scale(pygame.image.load("pictures/menu_bg.jpg"), (1280, 960)), (0,0))
+        player.draw(screen)
+        player.actions(screen, kb_input)
+        
+    if how_to == True:
+        ui.draw_howto(screen, mouse_x, mouse_y, font)
+
+    if Map_Shown == True:
+        map_Sprite_Group.draw(screen)
+
+    # Main menu
+    menu_input = ui.menu_system(mouse_x, mouse_y)
+    if start_game == False and how_to == False:
+        ui.update_mm()
+        ui.draw_mm(screen, mouse_x, mouse_y)
+    if menu_input == "QUIT":
+        quit()
+    if menu_input == "START":
+        start_game = True
+    if menu_input == "HOWTO":
+        how_to = True
+    if menu_input == "MAIN_MENU":
+        how_to = False
+        start_game = False
+
+while True:
+
+    # set max ticks per second (FPS)
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_ESCAPE]:
+            quit()
+        if event.type == pygame.key.get_pressed()[pygame.K_F3]:
+            Map_Shown = not Map_Shown
+
+
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    keyboard_input = pygame.key.get_pressed()
+
+    draw_gamewindow(screen, mouse_x, mouse_y, keyboard_input)
+    
+    pygame.display.update()
--- map_logic.py
+++ map_logic.py
@@ -20,12 +20,7 @@ if not pygame.mixer.get_init():
 #constants
 _Multiplier = 1
 SCREEN_WIDTH = 1280 * _Multiplier
-SCREEN_HEIGHT = 1024 * _Multiplier
-FPSCLOCK = pygame.time.Clock() #sets up a clock used for throttleing the fps
-BOARDWIDTH = 12
-BOARDHEIGHT = 7
-TILESIZE = 100
-FPS = 60
+SCREEN_HEIGHT = 960 * _Multiplier
 BLANK = None
 DEBUG = False
 
@@ -63,50 +58,150 @@ def get_image(path):
 
 #follow the rabit hole, all classes run their calls to functions in game and main talks to game
 
-def screen_Grid(screen, columns, rows,_Multiplier):
 
-    width = 80 *_Multiplier
-    height = 80 *_Multiplier
-    margin = round(2 *_Multiplier)
-    upper_Left_X = screen.get_width() - (((columns +1) * width) + ((columns + 2) * margin))
-    upper_Left_Y = screen.get_height() - (((rows + 2) * height) + ((rows + 2) * margin))
-    lower_Right_Y = upper_Left_Y + (rows * height) + ((rows + 1) * margin)
-    lower_Right_X = upper_Left_X + (columns * width) + (columns * margin)
-    lower_Left_Y = lower_Right_Y
-    lower_Left_X = upper_Left_X
-    upper_Right_X = lower_Right_X
-    upper_Right_Y = upper_Left_Y
+#part of the map
+
+class Game_Map(pygame.sprite.Sprite):
+    def __init__(self, map_Sprite_Group, _Multiplier, screen):
+        pygame.sprite.Sprite.__init__(self)
+        self.trail_Points, self.end_Point, self.start = self.generate_Map(screen, _Multiplier, map_Sprite_Group)
+        self.list_Of_Poi = self.poi_Generation(self.trail_Points)
 
 
+    def screen_Grid(self, screen, columns, rows,_Multiplier):
 
-    pygame.draw.rect(screen, (WHITE), ((upper_Left_X, upper_Left_Y), (lower_Right_X - (height //2) - (margin * 3), lower_Right_Y - (width //2))))
-    pygame.draw.line(screen, BLACK, (upper_Left_X, upper_Left_Y), (upper_Right_X, upper_Right_Y), margin)
-    for row in range(rows):
-        for column in range(columns):
-            pygame.draw.line(screen, BLACK, (upper_Left_X, upper_Left_Y + (row * height) + (row * margin)),
-                             (upper_Right_X, upper_Right_Y + (row * height) + (row * margin)))  # horizontal line
-            pygame.draw.line(screen, BLACK, (upper_Left_X + (column * width) + (column * margin), upper_Left_Y), ((upper_Left_X + (column * width) + (column * margin) , lower_Left_Y)))  # vertical line
-            if DEBUG:
-                print("Upper x coord")
-                print(upper_Left_X + (column * width) + (column * margin))
-                print("Upper y coord")
-                print(upper_Left_Y)
-                print("Lower x coord")
-                print((upper_Left_X + (column * width) + (column * margin)))
-                print("Lower y coord")
-                print(lower_Left_Y)
-                print("other y is")
-                print((upper_Right_X, upper_Right_Y + (row * height) + (row * margin)))
-
-    pygame.draw.line(screen, BLACK, (upper_Right_X, upper_Right_Y), (lower_Right_X, lower_Right_Y), margin)
-    pygame.draw.line(screen, BLACK, (lower_Left_X, lower_Left_Y), (lower_Right_X, lower_Right_Y), margin)
+        width = 80 *_Multiplier
+        height = 80 *_Multiplier
+        margin = round(2 *_Multiplier)
+        upper_Left_X = screen.get_width() - (((columns +1) * width) + ((columns + 2) * margin))
+        upper_Left_Y = screen.get_height() - (((rows + 1) * height) + ((rows + 2) * margin))
+        lower_Right_Y = upper_Left_Y + (rows * height) + ((rows + 1) * margin)
+        lower_Right_X = upper_Left_X + (columns * width) + (columns * margin)
+        lower_Left_Y = lower_Right_Y
+        lower_Left_X = upper_Left_X
+        upper_Right_X = lower_Right_X
+        upper_Right_Y = upper_Left_Y
 
 
 
+        pygame.draw.rect(screen, (WHITE), ((upper_Left_X, upper_Left_Y), (lower_Right_X - (width //2) - (margin * 3), lower_Right_Y - (height //2) - (margin * 8))))
+        pygame.draw.line(screen, BLACK, (upper_Left_X, upper_Left_Y), (upper_Right_X, upper_Right_Y), margin)
+        for row in range(rows):
+            for column in range(columns):
+                pygame.draw.line(screen, BLACK, (upper_Left_X, upper_Left_Y + (row * height) + (row * margin)),
+                                 (upper_Right_X, upper_Right_Y + (row * height) + (row * margin)))  # horizontal line
+                pygame.draw.line(screen, BLACK, (upper_Left_X + (column * width) + (column * margin), upper_Left_Y), ((upper_Left_X + (column * width) + (column * margin) , lower_Left_Y)))  # vertical line
+
+        pygame.draw.line(screen, BLACK, (upper_Right_X, upper_Right_Y), (lower_Right_X, lower_Right_Y), margin)
+        pygame.draw.line(screen, BLACK, (lower_Left_X, lower_Left_Y), (lower_Right_X, lower_Right_Y), margin)
 
 
+    def generate_Map(self, screen, _Multiplier, map_Sprite_Group):
+        map_Grid = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+
+        margin = 2
+        num_Of_Rows = 10
+        num_Of_Col = 14
+
+        # starting node's locational data
+        start_x = 1114
+        selection = [40, 122, 204, 286, 368, 450, 532, 614, 696, 778]
+        start_y = random.choice(selection)
+        self.start = node(start_x, start_y, './images/placeholder/start.png', screen, _Multiplier)
+        map_Sprite_Group.add(self.start)
+
+        # trail logic
+        o_Trail = road(screen, _Multiplier, start_x, start_y)
+        trail_Nodes = []
+        trail_Nodes.append(o_Trail.moving())
+
+        while trail_Nodes[-1][0] > 122:
+            trail_Nodes.append(o_Trail.moving())
+
+        # end point creation
+        self.end_Point = node(50, trail_Nodes[-1][1], './images/placeholder/end.png', screen, _Multiplier)
+        map_Sprite_Group.add(self.end_Point)
+
+        return trail_Nodes, self.end_Point, self.start
 
 
+    def poi_Generation(self, trail_Nodes):
+        # poi generation
+        num_Of_Poi = random.randint(12, 20)
+        names_Of_Poi = ["Hunter's Quarry", "Johnson's Sack", "Havagard's Hole", "Rustic Peak", "EastWard Tumble",
+                        "Frank's Redhot",
+                        "William's Child", "Magestic Gultch", "Trader's Point", "Kirkland's Endevor", "Widow's Peak",
+                        "Chicken Run",
+                        "Dodge City", "Angel's Howl", "Desolation Vale", "Jaggedally", "Violence Worth", "Barehallow",
+                        "Bonetooth",
+                        "Hope's Peak", "Wrath Brook", "Sandflats", "Talon's River", "Bruisestead", "Harmony Crag",
+                        "Snake Canyon",
+                        "Lightbrook", "Skullalley", "Phantom Rock"
+                        ]
+        closest_Point = random.sample(trail_Nodes, num_Of_Poi)
+        dir = 'not'
+
+        self.list_Of_Poi = []
+
+        for i in range(len(closest_Point)):
+            r = random.randint(0, 10)
+
+            if r % 2 == 0:
+                y_coord = closest_Point[i][1] + random.randint(0, 15)
+                dir = 'positive'
+            else:
+                y_coord = closest_Point[i][1] - random.randint(0, 15)
+                dir = 'negative'
+            x_coord = closest_Point[i][0]
+            name_Choice = random.choice(names_Of_Poi)
+            self.list_Of_Poi.append((name_Choice, (x_coord, y_coord), dir))
+            names_Of_Poi.remove(name_Choice)
+
+        return self.list_Of_Poi
+
+
+    def draw_Poi(self, screen):
+        for i in range(len(self.list_Of_Poi)):
+            print(i)
+            x_coord = self.list_Of_Poi[i][1][0]
+            y_coord = self.list_Of_Poi[i][1][1]
+            pygame.draw.circle(screen, GREEN, (x_coord, y_coord), 5)
+            font = pygame.freetype.Font(None, 10)
+            dir = self.list_Of_Poi[i][2]
+            name_Choice = self.list_Of_Poi[i][0]
+            if dir == 'positive':
+                font.render_to(screen, (x_coord + 10, y_coord + 10), name_Choice)
+            elif dir == 'negative':
+                font.render_to(screen, (x_coord - 10, y_coord - 10), name_Choice)
+
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        overlay = pygame.Surface((screen.get_width(), screen.get_height()))
+        overlay.set_alpha(150)
+        overlay.fill((WHITE))
+        screen.blit(overlay, (0, 0))
+        screen_Grid(screen, 14, 10, _Multiplier)
+        draw_Poi(screen)
+        pygame.draw.line(screen, DK_PURPLE, (trail_Points[0][0], trail_Points[0][1]), (start.return_Coords()))
+        for i in range(len(trail_Points) - 2):
+            pygame.draw.line(screen, DK_PURPLE, (trail_Points[i][0], trail_Points[i][1]),
+                             (trail_Points[i + 1][0], trail_Points[i + 1][1]))
+        pygame.draw.line(screen, DK_PURPLE, (trail_Points[-2][0], trail_Points[-2][1]), (end_Point.return_Coords()))
+        draw_Poi(list_Of_Poi, screen)
 
 class node(pygame.sprite.Sprite):
     def __init__(self, x, y, img, screen, _Multiplier):
@@ -156,160 +251,3 @@ class road():
         self.road_Points.append((self.x, self.y))
         return self.road_Points[-1][0], self.road_Points[-1][1]
 
-
-
-
-
-
-
-
-
-
-
-def generate_Map(screen, _Multiplier, map_Sprite_Group):
-    map_Grid = [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ]
-
-    margin = 2
-    num_Of_Rows = 10
-    num_Of_Col = 14
-
-    #starting node's locational data
-    start_x = 1114
-    selection = [40, 122, 204, 286, 368, 450, 532, 614, 696, 778]
-    start_y = random.choice(selection)
-    start = node(start_x, start_y, './images/placeholder/start.png', screen, _Multiplier)
-    map_Sprite_Group.add(start)
-
-    #trail logic
-    o_Trail = road(screen, _Multiplier, start_x, start_y)
-    trail_Nodes = []
-    trail_Nodes.append(o_Trail.moving())
-
-    while trail_Nodes[-1][0] > 122:
-        trail_Nodes.append(o_Trail.moving())
-
-    #end point creation
-    end_Point = node(50, trail_Nodes[-1][1], './images/placeholder/end.png', screen, _Multiplier)
-    map_Sprite_Group.add(end_Point)
-
-
-
-
-
-
-    return trail_Nodes, end_Point, start
-
-
-
-def poi_Generation(trail_Nodes):
-
-    # poi generation
-    num_Of_Poi = random.randint(12, 20)
-    names_Of_Poi = ["Hunter's Quarry", "Johnson's Sack", "Havagard's Hole", "Rustic Peak", "EastWard Tumble",
-                    "Frank's Redhot",
-                    "William's Child", "Magestic Gultch", "Trader's Point", "Kirkland's Endevor", "Widow's Peak",
-                    "Chicken Run",
-                    "Dodge City", "Angel's Howl", "Desolation Vale", "Jaggedally", "Violence Worth", "Barehallow",
-                    "Bonetooth",
-                    "Hope's Peak", "Wrath Brook", "Sandflats", "Talon's River", "Bruisestead", "Harmony Crag",
-                    "Snake Canyon",
-                    "Lightbrook", "Skullalley", "Phantom Rock"
-                    ]
-    closest_Point = random.sample(trail_Nodes, num_Of_Poi)
-    dir = 'not'
-
-    list_Of_Poi = []
-
-    for i in range(len(closest_Point)):
-        r = random.randint(0, 10)
-
-        if r % 2 == 0:
-            y_coord = closest_Point[i][1] + random.randint(0, 15)
-            dir = 'positive'
-        else:
-            y_coord = closest_Point[i][1] - random.randint(0, 15)
-            dir = 'negative'
-        x_coord = closest_Point[i][0]
-        name_Choice = random.choice(names_Of_Poi)
-        list_Of_Poi.append((name_Choice, (x_coord, y_coord), dir))
-        names_Of_Poi.remove(name_Choice)
-
-    return list_Of_Poi
-
-
-def draw_Poi(list_Of_Poi, screen):
-
-    for i in range(len(list_Of_Poi)):
-        print(i)
-        x_coord = list_Of_Poi[i][1][0]
-        y_coord = list_Of_Poi[i][1][1]
-        pygame.draw.circle(screen, GREEN, (x_coord, y_coord), 5)
-        font = pygame.freetype.Font(None, 10)
-        dir = list_Of_Poi[i][2]
-        name_Choice = list_Of_Poi[i][0]
-        if dir == 'positive':
-            font.render_to(screen, (x_coord + 10, y_coord + 10), name_Choice)
-        elif dir == 'negative':
-            font.render_to(screen, (x_coord - 10, y_coord - 10), name_Choice)
-
-
-
-
-
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-map_Sprite_Group = pygame.sprite.Group()
-
-def main(screen, _Multiplier, map_Sprite_Group):
-
-
-
-    pygame.display.set_caption('WW-TS')
-    pygame.mouse.set_visible(True)
-
-    #create our objects and set data
-    done = False
-    all_Sprite_Group = pygame.sprite.Group()
-    trail_Points, end_Point, start = generate_Map(screen, _Multiplier, map_Sprite_Group)
-    list_Of_Poi = poi_Generation(trail_Points)
-
-
-    while not done:
-        screen.fill(BLACK)
-        overlay = pygame.Surface((screen.get_width(), screen.get_height()))
-        overlay.set_alpha(150)
-        overlay.fill((WHITE))
-        screen.blit(overlay, (0,0))
-
-
-
-        pygame.event.pump()
-
-        screen_Grid(screen, 14, 10, _Multiplier)
-
-        map_Sprite_Group.draw(screen)
-        pygame.draw.line(screen, DK_PURPLE, (trail_Points[0][0], trail_Points[0][1]), (start.return_Coords()))
-        for i in range(len(trail_Points) - 2):
-           pygame.draw.line(screen, DK_PURPLE, (trail_Points[i][0], trail_Points[i][1]), (trail_Points[i+1][0], trail_Points[i+1][1]) )
-        pygame.draw.line(screen, DK_PURPLE, (trail_Points[-2][0], trail_Points[-2][1]), (end_Point.return_Coords()))
-        draw_Poi(list_Of_Poi, screen)
-
-        pygame.display.flip()
-
-        FPSCLOCK.tick(60)
-
-
-    pygame.quit() # closes window and exits
-
-if __name__ == '__main__':
-    main(screen,_Multiplier, map_Sprite_Group)
