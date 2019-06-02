26d57354cb246277d51ed9a8a42c771857612dd2
Psychicash <gnitsaf@gmail.com>
2019-06-02 19:51:59 -0400

    latest - counter in progress

--- game_entities.py
+++ game_entities.py
@@ -1,16 +1,41 @@
 import pygame
 import os
 
-class Player():
+#ASH - image function, loads image into an array and if it has already been loaded, it loads the previous loaded image
+#instead of wasting memory on a new image. if it hasn't been it loads it.
+_image_library = {}
+
+def get_image(path):
+    global _image_library
+    image = _image_library.get(path)
+    if image == None:
+        canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
+        image = pygame.image.load(canonicalized_path).convert()
+    return image
+
+
+class Player(pygame.sprite.Sprite):
 
     def __init__(self, start_pos=[0,0]):
+        pygame.sprite.Sprite.__init__(self)
         self.start_pos = start_pos
         self.cur_pos = start_pos
-        self.frames = [pygame.image.load("character_frames/frame_1.gif"), pygame.image.load("character_frames/frame_2.gif"),
-                       pygame.image.load("character_frames/frame_3.gif"), pygame.image.load("character_frames/frame_4.gif")]
+        self.images = []
+        self.load_images()
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        self.rect.x = start_pos[0]
+        self.rect.y = start_pos[1]
         self.walkcount = 0
         self.reverse_frames = False
         self.player_facing = 0
+        self.speed = 12
+
+    def load_images(self):
+        self.images.append(get_image("character_frames/frame1.png"))
+        self.images.append(get_image("character_frames/frame2.png"))
+        self.images.append(get_image("character_frames/frame3.png"))
+        self.images.append(get_image("character_frames/frame4.png"))
 
     def cycle_animation(self):
 
@@ -24,35 +49,43 @@ class Player():
         if self.walkcount == 0 and self.reverse_frames == True:
             self.reverse_frames = False
 
+
     def actions(self, screen, get_key):
 
         if pygame.key.get_pressed()[pygame.K_w]:
-            self.cur_pos[1] -= 10
+            self.rect.y -= self.speed
             self.cycle_animation()
             self.player_facing = 0
 
         if pygame.key.get_pressed()[pygame.K_s]:
-            self.cur_pos[1] += 10
+            self.rect.y += self.speed
             self.cycle_animation()
             self.player_facing = 2
+
         
         if pygame.key.get_pressed()[pygame.K_a]:
-            self.cur_pos[0] -= 10
+            self.rect.x -= self.speed
             self.cycle_animation()
             self.player_facing = 1
 
         if pygame.key.get_pressed()[pygame.K_d]:
-            self.cur_pos[0] += 10
+            self.rect.x += self.speed
             self.cycle_animation()
             self.player_facing = 3
     
-    def draw(self, screen):
+    def update(self):
 
         if self.player_facing == 0:
-            screen.blit(self.frames[self.walkcount], self.cur_pos)
-        if self.player_facing == 1:
-            screen.blit(pygame.transform.rotate(self.frames[self.walkcount], 90), self.cur_pos)
-        if self.player_facing == 2:
-            screen.blit(pygame.transform.rotate(self.frames[self.walkcount], 180), self.cur_pos)
-        if self.player_facing == 3:
-            screen.blit(pygame.transform.rotate(self.frames[self.walkcount], -90), self.cur_pos)
+            self.image = self.images[self.walkcount]
+        elif self.player_facing == 1:
+            self.image = self.images[self.walkcount]
+            self.image = pygame.transform.rotate(self.images[self.walkcount], 90)
+        elif self.player_facing == 2:
+            self.image = self.images[self.walkcount]
+            self.image = pygame.transform.rotate(self.images[self.walkcount], 180)
+        elif self.player_facing == 3:
+            self.image = self.images[self.walkcount]
+            self.image = pygame.transform.rotate(self.images[self.walkcount], -90)
+
+    def draw(self, screen):
+        pass
