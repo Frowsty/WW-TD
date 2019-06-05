f3825b21f925561fe2011788b0994f2d8fe9aed8
Frowsty <daniel_barbis@hotmail.com>
2019-06-05 18:13:51 +0100

    Finished the combat mechanics including enemies chasing the player and collision checks, also added Fullscreen support and changed some sprites

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import os
 from os import path
 import map_logic
 
-#initializers
+# initializers
 if __name__ == 'main':
     pygame.init()
 if not pygame.display.get_init():
@@ -21,14 +21,12 @@ pygame.font.init()
 if not pygame.mixer.get_init():
     pygame.mixer.init()
 
-
-#sprite groups
+# sprite groups
 all_Sprite_Group = pygame.sprite.Group()
 map_Sprite_Group = pygame.sprite.Group()
 terrain_sprites = pygame.sprite.Group()
 mpi_Group = pygame.sprite.Group()
 player_Sprite_Group = pygame.sprite.Group()
-projectile_Sprite_Group = pygame.sprite.Group()
 enemy_Sprite_Group = pygame.sprite.Group()
 
 # Define colors
@@ -47,19 +45,17 @@ PURPLE = (153, 51, 255)
 DK_PURPLE = (102, 0, 204)
 BROWN = (204, 153, 0)
 
-
 # Set screen size
 size = [1280, 960]
 screen = pygame.display.set_mode(size)
 
-#ash's constants
+# ash's constants
 _Multiplier = 1
 SCREEN_WIDTH = 1280 * _Multiplier
 SCREEN_HEIGHT = 1024 * _Multiplier
 BLANK = None
 DEBUG = False
 Map_Shown = False
-
 
 # Set window title
 pygame.display.set_caption("WW - TD")
@@ -73,9 +69,10 @@ ammo_font = pygame.font.SysFont("Arial", 30)
 start_game = False
 how_to = False
 
-#ASH - image function, loads image into an array and if it has already been loaded, it loads the previous loaded image
-#instead of wasting memory on a new image. if it hasn't been it loads it.
+# ASH - image function, loads image into an array and if it has already been loaded, it loads the previous loaded image
+# instead of wasting memory on a new image. if it hasn't been it loads it.
 _image_library = {}
+
 
 def get_image(path):
     global _image_library
@@ -85,6 +82,7 @@ def get_image(path):
         image = pygame.image.load(canonicalized_path).convert()
     return image
 
+
 def get_image_convert_alpha(path):
     global _image_library
     image = _image_library.get(path)
@@ -92,6 +90,18 @@ def get_image_convert_alpha(path):
         canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
         image = pygame.image.load(canonicalized_path).convert_alpha()
     return image
+
+# toggle fullscreen (ON/OFF)
+def toggle_fullscreen(fullscreen):
+    if fullscreen:
+        screen = pygame.display.set_mode(
+            (size[0], size[1]), pygame.RESIZABLE
+        )
+    else:
+        screen = pygame.display.set_mode(
+            (size[0], size[1]), pygame.FULLSCREEN
+        )
+    return not fullscreen
 
 
 # Initialize our player
@@ -105,28 +115,37 @@ map = map_logic.GameMapController(map_Sprite_Group, _Multiplier, screen, terrain
 map_Sprite_Group.add(map)
 
 menu_bg = pygame.transform.scale(get_image("pictures/menu_bg.jpg"), (1280, 960))
-bullet_img = pygame.transform.scale(get_image_convert_alpha("pictures/bullet.png"), (15, 15))
+bullet_img = pygame.transform.scale(get_image_convert_alpha("pictures/bullet.png"), (10, 10))
 shell_img = get_image_convert_alpha("pictures/shell.png")
 
-def draw_gamewindow(screen, mouse_x, mouse_y, kb_input):
-    global start_game, how_to
+enemies = [entities.Enemy([500, 300]), entities.Enemy([700, 300])]
+bullet_hit_tick = 0
+
+def draw_gamewindow(screen, mouse_x, mouse_y, kb_input, fps):
+    global start_game, how_to, was_pushed
     screen.fill(BLACK)
 
     if start_game == True:
-        screen.blit(menu_bg, (0,0))
-        ui.ingame_interface(screen, mouse_x, mouse_y, player.bullets, ammo_font, clock, shell_img)
-        player.draw(screen, ammo_font, mouse_x, mouse_y)
+        screen.blit(menu_bg, (0, 0))
+        ui.ingame_interface(screen, mouse_x, mouse_y, player.bullets, ammo_font, font, clock, shell_img)
+        player.draw(screen, ammo_font)
         player.actions(ui.auto_reload.get_state())
-        enemy.draw(screen)
+        for enemy in enemies:
+            enemy.draw(screen, player.cur_pos, player.player_frames[0], fps, player.dead)
+            if enemy.hit_player == True:
+                player.health -= 15
+                player.cur_pos += pygame.math.Vector2(75, 0).rotate(-enemy.direction)
+                enemy.hit_player = False
 
         for bullet in player.bullets:
             bullet.draw(screen, bullet_img)
-            if projectile_Sprite_Group.has(bullet):
-                projectile_Sprite_Group.remove(bullet)
-            else:
-                projectile_Sprite_Group.add(bullet)
-        #todo check bullets for sprite status, add them to their own group, add group to update and draw functions
-        
+            for enemy in enemies:
+                bullet.collision_check(enemy.cur_pos, enemy.player_frame)
+                if bullet.hit_target == True:
+                    enemy.health -= bullet.damage
+                    enemies.pop(enemies.index(enemy))
+                    bullet.hit_target = False
+
     if how_to == True:
         ui.draw_howto(screen, mouse_x, mouse_y, font, start_game, menu_bg)
 
@@ -136,7 +155,6 @@ def draw_gamewindow(screen, mouse_x, mouse_y, kb_input):
             sprite.draw(screen)
         map_Sprite_Group.draw(screen)
         mpi_Group.draw(screen)
-
 
     # Main menu
     menu_input = ui.menu_system(mouse_x, mouse_y, start_game)
@@ -153,29 +171,38 @@ def draw_gamewindow(screen, mouse_x, mouse_y, kb_input):
         how_to = False
         start_game = False
 
+fullscreen = False
+
 while True:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_ESCAPE]:
             quit()
-    
+        if pygame.key.get_pressed()[pygame.K_F12]:
+            fullscreen = toggle_fullscreen(fullscreen)
+
         if pygame.key.get_pressed()[pygame.K_F3]:
-                Map_Shown = not Map_Shown
-                sleep(0.10)
+            Map_Shown = not Map_Shown
+            player.bullets.clear()
+            enemies.clear()
+            sleep(0.10)
         if Map_Shown:
             if pygame.key.get_pressed()[pygame.K_m]:
                 for sprite in mpi_Group:
                     sprite.toggle_movement()
-            mpi_Group.update()
-
+                sleep(0.10)
+            mpi_Group.update(screen)
 
     mouse_x, mouse_y = pygame.mouse.get_pos()
     keyboard_input = pygame.key.get_pressed()
 
+    fps = clock.tick(60) / 1000.0
+
+    #player_Sprite_Group.draw(screen)
+    draw_gamewindow(screen, mouse_x, mouse_y, keyboard_input, fps)
+
+
     all_Sprite_Group.update()
-    
-    draw_gamewindow(screen, mouse_x, mouse_y, keyboard_input)
-    
     pygame.display.update()
     # set max ticks per second (FPS)
     clock.tick(60)
