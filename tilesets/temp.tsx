8170e0d4c59e8b1e469e1d5310b463fab684fd9a
Psychicash <gnitsaf@gmail.com>
2019-06-06 22:37:24 -0400

    camera working

---
+++ tilesets/temp.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="temp" tilewidth="16" tileheight="16" tilecount="252" columns="14">
+ <image source="Sprite-0001.png" width="225" height="300"/>
+</tileset>
---
+++ tilesets/try2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="try2" tilewidth="64" tileheight="64" tilecount="49" columns="7">
+ <image source="try2.png" width="500" height="500"/>
+</tileset>
